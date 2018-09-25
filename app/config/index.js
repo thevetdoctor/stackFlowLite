79848bd6510ced2b52acfd18bbe3ac8629b0629c
oba <thevetdoctor@gmail.com>
2018-09-25 03:41:55 +0100

    heroku postgres config

--- app/config/index.js
+++ app/config/index.js
@@ -1,11 +1,32 @@
 // index.js
 
+// const config = {
+// 	host: 'localhost',
+// 	user: 'postgres',
+// 	database: 'stackflow',
+// 	password: 'animalworld',
+// 	port: 5433
+// }
+
 const config = {
-	host: 'localhost',
-	user: 'postgres',
-	database: 'stackflow',
-	password: 'animalworld',
-	port: 5433
+	host: 'ec2-54-227-241-179.compute-1.amazonaws.com',
+	user: 'hxfwidjpablyft',
+	database: 'deqh5su4lbddfe',
+	password: '92796c0f5884d191db666773f548f152be09b88dbf20916343e02af75a78f282',
+	port: 5432
 }
+
+// Host
+// ec2-54-227-241-179.compute-1.amazonaws.com
+// Database
+// deqh5su4lbddfe
+// User
+// hxfwidjpablyft
+// Port
+// 5432
+// Password
+// 92796c0f5884d191db666773f548f152be09b88dbf20916343e02af75a78f282
+// URI
+// postgres://hxfwidjpablyft:92796c0f5884d191db666773f548f152be09b88dbf20916343e02af75a78f282@ec2-54-227-241-179.compute-1.amazonaws.com:5432/deqh5su4lbddfe'
 
 module.exports = config;
